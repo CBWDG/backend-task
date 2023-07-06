fdcc52777affeff263dec52edacf87316e6431da
CBWDG <zaratole@naver.com>
2023-07-07 08:06:10 +0900

    백엔드에서 로그인 기능 구현

--- login-lecture/app/src/routes/home/home.ctrl.js
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -10,11 +10,31 @@ const output = {
    },
    };
 
+   const users = {
+      id: ["WDG", "너도개발", "원팀장"],
+      psword: ["1234", "1234", "123456"],
+   };
+   
    const process = {
       login: (req, res) => {
-         console.log(req.body);
-      }
-   }
+         const id = req.body.id
+            psword = req.body.psword;
+
+         if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+               return res.json({
+                  success: true,
+               });
+            }
+         }
+
+         return res.kson({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+         })
+      },
+   };
 
 module.exports = {
    output,
