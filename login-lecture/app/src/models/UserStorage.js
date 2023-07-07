c0d08a2c2b91891519ce4dc9281616d4ba7b459c
CBWDG <zaratole@naver.com>
2023-07-07 10:10:46 +0900

    회원가입 라우팅 구현

--- login-lecture/app/src/models/UserStorage.js
+++ login-lecture/app/src/models/UserStorage.js
@@ -17,17 +17,26 @@ class UserStorage {
     }, {});
     return newUsers;
   }
-  static getUserInfo(id) {
-   const users = this.#users;
-   const idx = users.id.indexOf(id);
-   const usersKeys = Object.keys(users); // => [id, psword, name]
-   const userInfo = usersKeys.reduce((newUser, info) => {
-     newUser[info] = users[info][idx];
-     return newUser;
-   }, {});
+    static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // => [id, psword, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
 
-   return userInfo;
-   }
+    return userInfo;
+  }
+
+  static save(userInfo) {
+    const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.psword.push(userInfo.psword);
+    return { success: true };
+  }
+
 }
 
 module.exports = UserStorage;
